8375eacbf4f7ed3b8dd6545645190b461bc19948
dovbysh1 <ydovbyshenko@gmail.com>
2025-04-07 19:17:26 +0300

    Merge branch 'develop'

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,7 +5,6 @@ public class Main {
     private static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App.");
         System.out.println("App for measures converting.");
         System.out.println("Version 1.0.");
 
